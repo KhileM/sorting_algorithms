a7aaa43e6f3373c3208db5933668eab557f8b5a1
Themba <thembaradebe172@gmail.com>
2023-07-25 15:00:46 -0700

    correction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,39 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list via insertion sort
- * @list: The head of the list
+ * insertion_sort_list- sorts a doubly linked list via insertion sort
+ * @list: is the head of the list
  * Return: void
  */
-
 void insertion_sort_list(listint_t **list)
 {
-listint_t *ptr = NULL, *tmp = NULL;
+	listint_t *ptr = NULL, *tmp = NULL;
 
-if (list == NULL || *list == NULL || (*list)->next == NULL)
-return (void);
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-ptr = *list;
-ptr = ptr->next;
-while (ptr)
-{
-for (; ptr->prev && ptr->n < (ptr->prev)->n; ptr = ptr->prev)
-{
-tmp = ptr;
-if (ptr->next)
-(ptr->next)->prev = tmp->prev;
-(ptr->prev)->next = tmp->next;
-ptr = ptr->prev;
-tmp->prev = ptr->prev;
-tmp->next = ptr;
-if (ptr->prev)
-(ptr->prev)->next = tmp;
-ptr->prev = tmp;
-if (tmp->prev == NULL)
-*list = tmp;
-print_list(*list);
-}
-ptr = ptr->next;
-}
+	ptr = *list;
+	ptr = ptr->next;
+	while (ptr)
+	{
+		while (ptr->prev && ptr->n < (ptr->prev)->n)
+		{
+			tmp = ptr;
+			if (ptr->next)
+				(ptr->next)->prev = tmp->prev;
+			(ptr->prev)->next = tmp->next;
+			ptr = ptr->prev;
+			tmp->prev = ptr->prev;
+			tmp->next = ptr;
+			if (ptr->prev)
+				(ptr->prev)->next = tmp;
+			ptr->prev = tmp;
+			if (tmp->prev == NULL)
+				*list = tmp;
+			print_list(*list);
+			ptr = ptr->prev;
+		}
+		ptr = ptr->next;
+	}
+
 }
